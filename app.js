a477e32c5e456b2c85830afc2cce96e74a84b1b0
khyun-0123 <128883244+khyun-0123@users.noreply.github.com>
2023-12-16 18:58:51 +0900

    header edit, form edit
    헤더부분 전체 수정, 고객센터 기능 내에 글 작성하기 포함시켜 둠

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 
 
 
-const mysql = require('mysql')
+const mysql = require('mysql2')
 const connection = mysql.createConnection(process.env.DATABASE_URL)
 console.log('Connected to PlanetScale!')
 
